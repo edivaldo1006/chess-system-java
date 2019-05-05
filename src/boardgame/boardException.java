f0aa31897ea2b019a3e8539a92131c96e95467cb
Edivaldo Cabral <edivaldo.junior10@hotmail.com>
2019-05-05 19:11:49 -0300

    BoardException and defencive programming

---
+++ src/boardgame/boardException.java
@@ -0,0 +1,10 @@
+package boardgame;
+
+public class boardExeption extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public boardExeption(String msg) {
+		super(msg);
+	}
+
+}
